9a9f9586ab52c09067fabe2debfa9ecef612d93d
saraalinazari <sara.alinazari@gmail.com>
2018-05-26 13:39:44 -0700

    try to solve

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.port || 3002;
+const PORT = process.env.PORT || 3002;
 //const host = '0.0.0.0';
 
 app.use(bodyParser.urlencoded({ extended: true }));
