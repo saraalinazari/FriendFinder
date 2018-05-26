9a7b6c13dc465c08ae96f85f2b3a0371cfe45c93
saraalinazari <sara.alinazari@gmail.com>
2018-05-26 13:08:10 -0700

    to solve error

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.port || 2000;
-const host = '0.0.0.0';
+const PORT = process.env.port || 3000;
+//const host = '0.0.0.0';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT,host,function(){
+app.listen(PORT,function(){
     console.log("App listening on PORT:" + PORT);
 });
 
